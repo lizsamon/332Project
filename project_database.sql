d5d175ef2f822ef13d5007500e48b011adbb1885
lizsamon <lizsamon@csu.fullerton.edu>
2024-07-30 19:42:47 -0700

    set up the project_database for tables

--- project_database.sql
+++ project_database.sql
@@ -4,6 +4,25 @@ CREATE DATABASE project_database;
 --using the database
 USE project_database; 
 
+--Professor Table
+
+--Department
+
+--Student
+
+--Course
+
+--Section
+
+--College_degrees
+
+--Minors
+
+--Enrollment_Record
+
+--Prerequisites
+
+
 
 
 
